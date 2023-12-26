17bdc972af103602138bb8d86e8bfef14c5cbc93
raneemhamarneh <raneem99hamarneh@gmail.com>
2023-12-26 14:54:02 +0300

    done task

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+      },
+      body: JSON.stringify({
+          name: name,
+          email: email,
+      }),
+  })
+  .then(response => response.json())
+  .then(data => {
+      // Append the received ID to the DOM
+      const newElement = document.createElement('p');
+      newElement.textContent = `ID: ${data.id}`;
+      document.body.appendChild(newElement);
+  })
+  .catch(error => {
+      // Append the error message to the DOM
+      const errorElement = document.createElement('p');
+      errorElement.textContent = `Error: ${error.message}`;
+      document.body.appendChild(errorElement);
+  });
+}
